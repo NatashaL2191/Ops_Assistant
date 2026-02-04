2733622c3e5896f0f6a94ecffda1a6a29ba75233
Natasha Linares <linaresn2024@gmail.com>
2026-02-03 20:45:26 -0500

    Added File Structure

---
+++ Application_Layer/op_assistant.py
@@ -0,0 +1,16 @@
+class OpAssistant:
+    def __init__(self):
+        
+        pass
+    def parse_time_window():
+        pass
+    def classify_query():
+        pass
+    def generate_sql():
+        pass
+    def format_answer():
+        pass
+    def process_query():
+        pass
+    def close():
+        pass
