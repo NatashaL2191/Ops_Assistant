7960672de9350af456f59454687f408aef2a0a1b
Natasha Linares <linaresn2024@gmail.com>
2026-02-03 21:08:46 -0500

    Created tables in schema.sql

--- Data_Layer/schema.sql
+++ Data_Layer/schema.sql
@@ -1,7 +1,38 @@
 CREATE TABLE zones(
-    zone_id TEXT PRIMARY KEY,
+    zone_ID TEXT PRIMARY KEY,
     zone_name TEXT NOT NULL,
     floor INTEGER NOT NULL,
     zone_type TEXT,
     description TEXT
+);
+CREATE TABLE entities(
+    entity_ID TEXT PRIMARY KEY,
+    entity_name TEXT NOT NULL,
+    entity_type TEXT,
+    department TEXT,
+    active BOOLEAN DEFAULT 1
+
+);
+
+CREATE TABLE pings(
+    ping_ID INTEGER PRIMARY KEY,
+    entity_ID TEXT NOT NULL,
+    zone_ID TEXT NOT NULL,
+    timestamp DATETIME NOT NULL,
+    rssi INTEGER,
+    accuracy REAL NOT NULL,
+    FOREIGN KEY zone_ID REFERENCES zones(zone_ID),
+    FOREIGN KEY entity_ID REFERENCES entities(entity_ID)
+);
+
+CREATE TABLE derived_zones(
+    event_id TEXT PRIMARY KEY,
+    entity_ID TEXT NOT NULL,
+    zone_ID TEXT NOT NULL,
+    event_type TEXT NOT NULL,
+    timestamp DATETIME NOT NULL,
+    dwell_seconds INTEGER,
+    FOREIGN KEY zone_ID REFERENCES zones(zone_ID),
+    FOREIGN KEY entity_ID REFERENCES entities(entity_ID)
+
 );
